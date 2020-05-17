18bd645d958e1ce6cc5d795ebed2798fa7fafe52
Liri CI <ci@liri.io>
2020-05-17 09:55:12 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/liri-screenshot_it_IT.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.1">
+<context>
+    <name>ScreenshotClient</name>
+    <message>
+        <location filename="../src/screenshot/screenshotclient.cpp" line="205"/>
+        <source>Screenshot from %1</source>
+        <translation>Screenshot da %1</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="37"/>
+        <source>Screenshot</source>
+        <translation>Screenshot</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="56"/>
+        <location filename="../src/screenshot/qml/main.qml" line="125"/>
+        <source>Screen capture</source>
+        <translation>Cattura schermo</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="60"/>
+        <source>Capture</source>
+        <translation>Cattura</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="74"/>
+        <source>Grab</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="80"/>
+        <source>All screens</source>
+        <translation>Tutti gli schermi</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="90"/>
+        <source>Delay</source>
+        <translation>Ritardo</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="101"/>
+        <source>Include pointer</source>
+        <translation>Includi puntatore</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="111"/>
+        <source>Include the window border</source>
+        <translation>Includi il bordo della finestra</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="129"/>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="137"/>
+        <source>Copy to clipboard</source>
+        <translation>Copia negli appunti</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="186"/>
+        <source>Save Screenshot As</source>
+        <translation>Salva screenshot come</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="189"/>
+        <source>Image files (*.jpg *.jpeg *.png)</source>
+        <translation>File immagine (*.jpg *.jpeg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="189"/>
+        <source>All files (*)</source>
+        <translation>Tutti i file (*)</translation>
+    </message>
+</context>
+</TS>
