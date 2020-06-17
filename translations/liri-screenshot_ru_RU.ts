a11415ca086c5df0a1dcd41c2bb4c1a0ea062d4d
Liri CI <ci@liri.io>
2020-06-17 00:19:30 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/liri-screenshot_ru_RU.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru_RU" version="2.1">
+<context>
+    <name>ScreenshotClient</name>
+    <message>
+        <location filename="../src/screenshot/screenshotclient.cpp" line="205"/>
+        <source>Screenshot from %1</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="37"/>
+        <source>Screenshot</source>
+        <translation>Скриншот</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="56"/>
+        <location filename="../src/screenshot/qml/main.qml" line="125"/>
+        <source>Screen capture</source>
+        <translation>Скриншот</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="60"/>
+        <source>Capture</source>
+        <translation>Снимок</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="74"/>
+        <source>Grab</source>
+        <translation>Взять</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="80"/>
+        <source>All screens</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="90"/>
+        <source>Delay</source>
+        <translation>Отложить</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="101"/>
+        <source>Include pointer</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="111"/>
+        <source>Include the window border</source>
+        <translation>Включить границы окна</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="129"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="137"/>
+        <source>Copy to clipboard</source>
+        <translation>Копировать в буфер обмена</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="186"/>
+        <source>Save Screenshot As</source>
+        <translation>Сохранить скриншот как</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="189"/>
+        <source>Image files (*.jpg *.jpeg *.png)</source>
+        <translation>Файлы изображений (*.jpg *.jpeg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../src/screenshot/qml/main.qml" line="189"/>
+        <source>All files (*)</source>
+        <translation>Все файлы (*)</translation>
+    </message>
+</context>
+</TS>
